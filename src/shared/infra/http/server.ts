a9796b72267803a7f16b3a922681cd656c1ba6a1
Willian Home <willian@wiki4fit.com.br>
2020-05-30 05:44:12 -0300

    refatoração para arquitetura SOLID

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,17 +1,22 @@
+/* eslint-disable no-console */
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
-import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
 
-import './database';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
+
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
 const port = 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
